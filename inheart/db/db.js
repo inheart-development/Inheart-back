47cf77b15ca622ddc1ddfc05a47c1cded299bb7a
MunJH <zero4125966@gmail.com>
2019-05-23 15:07:50 +0900

    multer수정

--- inheart/db/db.js
+++ inheart/db/db.js
@@ -4,7 +4,7 @@ const mysql = require('mysql');
 const con = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '1234',
+    password: 'zero8787',
     database: 'inheart'
 });
 con.connect(function (err) {
