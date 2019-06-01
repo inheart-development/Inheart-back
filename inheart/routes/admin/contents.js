c76dc44977b8395ad1c5967768d07eafa831cd65
심의진 <Kusinaga@cpe-172-89-15-182.socal.res.rr.com>
2019-06-01 20:04:26 +0900

    admin/contents 콘텐츠 추가 및 수정 + 파일업로드

--- inheart/routes/admin/contents.js
+++ inheart/routes/admin/contents.js
@@ -1,6 +1,31 @@
 const router = require("express").Router();
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+const audioDuration = require("get-audio-duration");
+const timeFormat = require("hh-mm-ss");
+//const moment = require("moment");
+//const momentDurationFormatSetUp = require("moment-duration-format");
 const con = require("../../db/db");
 const util = require("../../check/util");
+
+var upload = multer({
+    storage: multer.diskStorage({
+        //서버 디스크에 저장
+        destination(req, file, cb) {
+            cb(null, "sound/"); //사운드 저장 경로
+        },
+        filename(req, file, cb) {
+            const ext = path.extname(file.originalname); //파일의 확장자를 ext에 저장
+            cb(
+                null,
+                path.basename(file.originalname, ext) +
+                new Date().valueOf() +
+                ext
+            ); //파일이름+업로드날짜+확장자
+        }
+    })
+});
 
 router.get("/list", (req, res, next) => {
     con.query(
@@ -26,7 +51,9 @@ router.get("/list", (req, res, next) => {
 });
 
 router.get("/category/list", (req, res, next) => {
-    const { categoryNo } = req.query;
+    const {
+        categoryNo
+    } = req.query;
     con.query(
         "select * from contents where categoryNo = 1 order by contentsIndex;",
         [categoryNo],
@@ -51,7 +78,9 @@ router.get("/category/list", (req, res, next) => {
 });
 
 router.get("/", (req, res, next) => {
-    const { contentsNo } = req.query;
+    const {
+        contentsNo
+    } = req.query;
 
     con.query(
         "select * from contents where contentsNo = ?;",
@@ -76,92 +105,108 @@ router.get("/", (req, res, next) => {
     );
 });
 
-router.post("/", (req, res, next) => {
+router.post("/", upload.single("contents"), (req, res, next) => {
+    res.header("Access-Control-Allow-Headers", "multipart/form-data");
     const {
         categoryNo,
         contentsTitle,
-        contentsExplan,
-        contentsFile,
+        contentsExplain,
+        //contentsFile,
         contentsType
     } = req.body;
 
-    const contentsTime = "00:12:23"; //파일을 받아서 시간 확인 필요
+    const contentsFile = req.file.filename;
 
-    con.query(
-        "insert into contents values('0',?,?,?,?,?,?,'1');",
-        [
-            categoryNo,
-            contentsTitle,
-            contentsExplan,
-            contentsTime,
-            contentsFile,
-            contentsType
-        ],
-        (err, result, fields) => {
-            if (err) {
-                //에러체크
-                return res
-                    .status(400)
-                    .json(util.successFalse(err, "업데이트 실패"));
+    audioDuration.getAudioDurationInSeconds('sound/' + req.file.filename).then((duration) => {
+        const contentsTime = timeFormat.fromS(Math.ceil(duration), "hh:mm:ss");
+
+        con.query(
+            "insert into contents values('0',?,?,?,?,?,?,'1');",
+            [
+                categoryNo,
+                contentsTitle,
+                contentsExplain,
+                contentsTime,
+                contentsFile,
+                contentsType
+            ],
+            (err, result, fields) => {
+                if (err) {
+                    //에러체크
+                    return res
+                        .status(400)
+                        .json(util.successFalse(err, "업데이트 실패"));
+                }
+
+                if (result && result.length != 0) {
+                    //result 결과값이 있으면
+
+                    console.log(result);
+                    return res.status(200).json(util.successTrue(result));
+                } else {
+                    return res.sendStatus(204);
+                }
             }
-
-            if (result && result.length != 0) {
-                //result 결과값이 있으면
-
-                console.log(result);
-                return res.status(200).json(util.successTrue(result));
-            } else {
-                return res.sendStatus(204);
-            }
-        }
-    );
+        );
+    });
+    //console.log(req.file);
+    //const contentsTime = "00:12:23"; //파일을 받아서 시간 확인 필요
 });
 
-router.put("/", (req, res, next) => {
+router.put("/", upload.single("contents"), (req, res, next) => {
+    res.header("Access-Control-Allow-Headers", "multipart/form-data");
     const {
         categoryNo,
         contentsTitle,
-        contentsExplan,
-        contentsFile,
+        contentsExplain,
+        //contentsFile,
         contentsType,
         contentsNo
     } = req.body;
 
-    const contentsTime = "00:12:23"; //파일을 받아서 시간 확인 필요
+    //console.log(req.body);
 
-    con.query(
-        "update contents set categoryNo = ?,contentsTitle = ?,contentsExplan = ?,contentsTime = ?,contentsFile = ?,contentsType = ? where contentsNo = ?;",
-        [
-            categoryNo,
-            contentsTitle,
-            contentsExplan,
-            contentsTime,
-            contentsFile,
-            contentsType,
-            contentsNo
-        ],
-        (err, result, fields) => {
-            if (err) {
-                //에러체크
-                return res
-                    .status(400)
-                    .json(util.successFalse(err, "업데이트 실패"));
+    const contentsFile = req.file.filename;
+
+    audioDuration.getAudioDurationInSeconds('sound/' + req.file.filename).then((duration) => {
+        const contentsTime = timeFormat.fromS(Math.ceil(duration), "hh:mm:ss");
+
+        con.query(
+            "update contents set categoryNo = ?,contentsTitle = ?,contentsExplan = ?,contentsTime = ?,contentsFile = ?,contentsType = ? where contentsNo = ?;",
+            [
+                categoryNo,
+                contentsTitle,
+                contentsExplain,
+                contentsTime,
+                contentsFile,
+                contentsType,
+                contentsNo
+            ],
+            (err, result, fields) => {
+                if (err) {
+                    //에러체크
+                    return res
+                        .status(400)
+                        .json(util.successFalse(err, "업데이트 실패"));
+                }
+    
+                if (result && result.length != 0) {
+                    //result 결과값이 있으면
+    
+                    console.log(result);
+                    return res.status(200).json(util.successTrue(result));
+                } else {
+                    return res.sendStatus(204);
+                }
             }
-
-            if (result && result.length != 0) {
-                //result 결과값이 있으면
-
-                console.log(result);
-                return res.status(200).json(util.successTrue(result));
-            } else {
-                return res.sendStatus(204);
-            }
-        }
-    );
+        );
+    });
 });
 
 router.delete("/", (req, res, next) => {
-    const { contentsNo } = req.body;
+    const {
+        contentsNo
+    } = req.body;
 
     con.query(
         "delete from contents where contentsNo = ?;",
