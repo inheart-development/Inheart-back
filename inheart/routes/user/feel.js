c49555821ff1d8ea4c8b20759203a3af523fdeb1
SoohanBae <soohan530@gmail.com>
2019-06-02 18:29:11 +0900

    feel 변경

--- inheart/routes/user/feel.js
+++ inheart/routes/user/feel.js
@@ -9,23 +9,6 @@ const auth = require("./auth")();
 
 const { isLoggedIn } = require("../../check/check");
 
-const upload2 = multer({
-    storage: multer.diskStorage({
-        destination(req, file, cb) {
-            cb(null, "feelImage/");
-        },
-        filename(req, file, cb) {
-            const ext = path.extname(file.originalname); //파일의 확장자를 ext에 저장
-            cb(
-                null,
-                path.basename(file.originalname, ext) +
-                    new Date().valueOf() +
-                    ext
-            ); //파일이름+업로드날짜+확장자
-        }
-    })
-});
-
 router.get("/", auth.authenticate(), (req, res, next) => {
     const { feelNo } = req.query;
 
@@ -152,46 +135,33 @@ router.get("/type/list", auth.authenticate(), (req, res, next) => {
     );
 });
 
-router.post(
-    "/",
-    auth.authenticate(),
-    isLoggedIn,
-    upload2.single("feelImage"),
-    (req, res, next) => {
-        const { contentsNo, feelText, feelTime, feelType } = req.body;
+router.post("/", auth.authenticate(), (req, res, next) => {
+    const { contentsNo, feelText, feelTime, feelTypeNo, feelTitle } = req.body;
 
-        const userNo = req.user.userNo;
+    const userNo = req.user.userNo;
 
-        if (userNo === -1)
-            return res
-                .status(401)
-                .json(
-                    util.successFalse(
-                        null,
-                        "토큰으로 부터 유저정보를 얻을 수 없습니다."
-                    )
-                );
+    if (userNo === -1)
+        return res
+            .status(401)
+            .json(
+                util.successFalse(
+                    null,
+                    "토큰으로 부터 유저정보를 얻을 수 없습니다."
+                )
+            );
 
-        let feelImgname = req.file.filename;
-        let q =
-            "insert into feel values('0','" +
-            userNo +
-            "','" +
-            contentsNo +
-            "','" +
-            new Date().toFormat("YYYY-MM-DD HH24:MI:SS") +
-            "','" +
-            feelImgname +
-            "','" +
-            "'0','" +
-            feelText +
-            "','" +
-            feelTime +
-            "','" +
-            feelType +
-            "')";
-        console.log(q);
-        con.query(q, (err, result, fields) => {
+    con.query(
+        "insert into feel values('0',?,?,?,?,?,?,?);",
+        [
+            userNo,
+            contentsNo,
+            new Date().toFormat("YYYY-MM-DD HH24:MI:SS"),
+            feelTitle,
+            feelText,
+            feelTime,
+            feelTypeNo
+        ],
+        (err, result, fields) => {
             if (err) {
                 //에러체크
                 return res
@@ -207,9 +177,9 @@ router.post(
             } else {
                 return res.sendStatus(204);
             }
-        });
-    }
-);
+        }
+    );
+});
 
 router.options("/", (req, res) => {
     res.sendStatus(200);
