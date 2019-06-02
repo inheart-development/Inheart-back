ba109919b755cdfec9ec42242c11dae22cc11a70
SoohanBae <soohan530@gmail.com>
2019-06-03 03:01:37 +0900

    와

--- inheart/app.js
+++ inheart/app.js
@@ -38,23 +38,23 @@ app.set("port", process.env.PORT || 3000);
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(
-  express.urlencoded({
-    extended: false
-  })
+    express.urlencoded({
+        extended: false
+    })
 );
 
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(
-  session({
-    name: "sessionID",
-    resave: false,
-    saveUninitialized: true,
-    secret: process.env.COOKIE_SECRET,
-    cookie: {
-      httpOnly: true,
-      secure: false
-    }
-  })
+    session({
+        name: "sessionID",
+        resave: false,
+        saveUninitialized: true,
+        secret: process.env.COOKIE_SECRET,
+        cookie: {
+            httpOnly: true,
+            secure: false
+        }
+    })
 );
 app.use(flash());
 app.use(passport.initialize()); //요청 객체에 passport설정을 심는다
@@ -66,27 +66,27 @@ app.use(passport.session()); //express-session에서 생성하는 것이므로 e
 // });
 
 //미들웨어 설정
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, Content-Type, X-Auth-Token, Authorization"
-  );
-  //res.header("Access-Control-Allow-Headers", "content-type");
-  res.header("X-Content-Type-Options", "nosniff");
-  res.header("X-Frame-Options", "deny");
-  // res.header("Content-Security-Policy", "default-src 'none'");
-  res.removeHeader("x-Powered-By");
-  //res.header("Content-Type", "application/json; charset=utf-8");
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+    );
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, Content-Type, X-Auth-Token, Authorization"
+    );
+    //res.header("Access-Control-Allow-Headers", "content-type");
+    res.header("X-Content-Type-Options", "nosniff");
+    res.header("X-Frame-Options", "deny");
+    // res.header("Content-Security-Policy", "default-src 'none'");
+    res.removeHeader("x-Powered-By");
+    //res.header("Content-Type", "application/json; charset=utf-8");
 
-  next();
+    next();
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // app.use('/', function (req, res, next) {
 //     console.log(req.headers)
@@ -119,18 +119,18 @@ app.use("/admin", adminRouter);
 //---------------------router------------------------
 
 app.use((req, res, next) => {
-  const err = new Error("Not Found");
-  err.status = 404;
-  return res.status(404).json(util.successFalse(err, "파일 찾을 수 없음"));
+    const err = new Error("Not Found");
+    err.status = 404;
+    return res.status(404).json(util.successFalse(err, "파일 찾을 수 없음"));
 });
 
 app.use((err, req, res) => {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-  res.status(err.status || 500);
-  res.render("error");
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+    res.status(err.status || 500);
+    res.render("error");
 });
 
 app.listen(app.get("port"), () => {
-  console.log(app.get("port"), "번 포트에서 대기 중");
+    console.log(app.get("port"), "번 포트에서 대기 중");
 });
