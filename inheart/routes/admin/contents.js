e693e88c0a3b4541b558bfbce4877ee7f62bee98
SoohanBae <soohan530@gmail.com>
2019-06-04 01:15:30 +0900

    컨텐츠 put

--- inheart/routes/admin/contents.js
+++ inheart/routes/admin/contents.js
@@ -18,7 +18,7 @@ var upload = multer({
             //console.log(type);
             if (type === "sound") {
                 cb(null, "meditation/sound");
-            } else if (fileType === "text") {
+            } else if (type === "text") {
                 cb(null, "meditation/text");
             } else {
                 console.log("multer error: undefined contents type used");
@@ -29,8 +29,8 @@ var upload = multer({
             cb(
                 null,
                 path.basename(file.originalname, ext) +
-                new Date().valueOf() +
-                ext
+                    new Date().valueOf() +
+                    ext
             ); //파일이름+업로드날짜+확장자
         }
     })
@@ -60,9 +60,7 @@ router.get("/list", (req, res, next) => {
 });
 
 router.get("/category/list", (req, res, next) => {
-    const {
-        categoryNo
-    } = req.query;
+    const { categoryNo } = req.query;
     con.query(
         "select * from contents where categoryNo = 1 order by contentsIndex;",
         [categoryNo],
@@ -86,9 +84,7 @@ router.get("/category/list", (req, res, next) => {
     );
 });
 router.get("/", (req, res, next) => {
-    const {
-        contentsNo
-    } = req.query;
+    const { contentsNo } = req.query;
 
     con.query(
         "select * from contents where contentsNo = ?;",
@@ -113,86 +109,95 @@ router.get("/", (req, res, next) => {
     );
 });
 
-router.post("/sound", upload.fields([{
-    name: "sound"
-}, {
-    name: "cover"
-}]), (req, res, next) => {
-    res.header("Access-Control-Allow-Headers", "multipart/form-data");
-    const {
-        categoryNo,
-        contentsTitle,
-        contentsExplain,
-        //contentsFile,
-        //contentsCover,
-        contentsType
-    } = req.body;
+router.post(
+    "/sound",
+    upload.fields([
+        {
+            name: "sound"
+        },
+        {
+            name: "cover"
+        }
+    ]),
+    (req, res, next) => {
+        res.header("Access-Control-Allow-Headers", "multipart/form-data");
+        const {
+            categoryNo,
+            contentsTitle,
+            contentsExplain,
+            //contentsFile,
+            //contentsCover,
+            contentsType
+        } = req.body;
 
-    if(contentsType !== "sound" && contentsType !== "text"){
-        return res
-            .status(400)
-            .json(
-                util.successFalse(
-                    err,
-                    "지정되지 않은 콘텐츠 타입을 사용했습니다"
-                )
-            );
+        contentsType = "sound";
+
+        if (contentsType !== "sound" && contentsType !== "text") {
+            return res
+                .status(400)
+                .json(
+                    util.successFalse(
+                        err,
+                        "지정되지 않은 콘텐츠 타입을 사용했습니다"
+                    )
+                );
+        }
+        console.log(req.files);
+        const contentsFile = req.files.sound[0].filename.normalize("NFC");
+        const contentsCover = req.files.cover[0].filename.normalize("NFC");
+
+        var contentsTime = "00:00:00";
+
+        console.log("type is sound");
+        audioDuration
+            .getAudioDurationInSeconds("meditation/sound/" + contentsFile)
+            .then(duration => {
+                contentsTime = timeFormat.fromS(
+                    Math.ceil(duration),
+                    "hh:mm:ss"
+                );
+
+                con.query(
+                    "insert into contents values('0',?,?,?,?,?,?,?,'1');",
+                    [
+                        categoryNo,
+                        contentsTitle,
+                        contentsExplain,
+                        contentsTime,
+                        contentsFile,
+                        contentsType,
+                        contentsCover
+                    ],
+                    (err, result, fields) => {
+                        if (err) {
+                            //에러체크
+                            return res
+                                .status(400)
+                                .json(util.successFalse(err, "업데이트 실패"));
+                        }
+
+                        if (result && result.length != 0) {
+                            //result 결과값이 있으면
+
+                            console.log(result);
+                            return res
+                                .status(200)
+                                .json(util.successTrue(result));
+                        } else {
+                            return res.sendStatus(204);
+                        }
+                    }
+                );
+            })
+            .catch(err => {
+                console.log(err);
+            });
     }
-    console.log(req.files);
-    const contentsFile = req.files.sound[0].filename.normalize("NFC");
-    const contentsCover = req.files.cover[0].filename.normalize("NFC");
-
-    var contentsTime = "00:00:00";
-
-    console.log("type is sound");
-    audioDuration
-        .getAudioDurationInSeconds("meditation/sound/" + contentsFile)
-        .then(duration => {
-            contentsTime = timeFormat.fromS(
-                Math.ceil(duration),
-                "hh:mm:ss"
-            );
-
-            con.query(
-                "insert into contents values('0',?,?,?,?,?,?,?,'1');",
-                [
-                    categoryNo,
-                    contentsTitle,
-                    contentsExplain,
-                    contentsTime,
-                    contentsFile,
-                    contentsType,
-                    contentsCover
-                ],
-                (err, result, fields) => {
-                    if (err) {
-                        //에러체크
-                        return res
-                            .status(400)
-                            .json(util.successFalse(err, "업데이트 실패"));
-                    }
-
-                    if (result && result.length != 0) {
-                        //result 결과값이 있으면
-
-                        console.log(result);
-                        return res
-                            .status(200)
-                            .json(util.successTrue(result));
-                    } else {
-                        return res.sendStatus(204);
-                    }
-                }
-            );
-        })
-        .catch(err => {
-            console.log(err);
-        });
-});
+);
 
 router.post("/text", upload.single("text"), (req, res, next) => {
     res.header("Access-Control-Allow-Headers", "multipart/form-data");
-    const {
+    var {
         categoryNo,
         contentsTitle,
         contentsExplain,
@@ -211,6 +216,8 @@ router.post("/text", upload.single("text"), (req, res, next) => {
             );
     }
     */
+
+    contentsType = "text";
     const contentsFile = req.file.filename.normalize("NFC");
     const contentsCover = "";
 
@@ -249,6 +256,208 @@ router.post("/text", upload.single("text"), (req, res, next) => {
     );
 });
 
+router.put(
+    "/sound",
+    upload.fields([
+        {
+            name: "sound"
+        },
+        {
+            name: "cover"
+        }
+    ]),
+    (req, res, next) => {
+        res.header("Access-Control-Allow-Headers", "multipart/form-data");
+        var {
+            categoryNo,
+            contentsTitle,
+            contentsExplain,
+            //contentsFile,
+            //contentsCover,
+            contentsType,
+            contentsNo
+        } = req.body;
+
+        contentsType = "sound";
+
+        var contentsFile = "";
+        var contentsCover = "";
+
+        if (req.files.sound != null) {
+            contentsFile =
+                ", contentsFile = '" +
+                req.files.sound[0].filename.normalize("NFC") +
+                "' ";
+        }
+
+        if (req.files.cover != null) {
+            console.log("커버입장!!!!!");
+            contentsCover =
+                ", contentsCover = '" +
+                req.files.cover[0].filename.normalize("NFC") +
+                "' ";
+        }
+
+        var contentsTime = "00:00:00";
+
+        if (req.files.sound != null) {
+            console.log("type is sound");
+            audioDuration
+                .getAudioDurationInSeconds(
+                    "meditation/sound/" +
+                        req.files.sound[0].filename.normalize("NFC")
+                )
+                .then(duration => {
+                    contentsTime = timeFormat.fromS(
+                        Math.ceil(duration),
+                        "hh:mm:ss"
+                    );
+
+                    console.log(req.files);
+
+                    console.log("type is sound");
+                    con.query(
+                        "update contents set categoryNo = ?,contentsTitle = ?,contentsExplain = ?,contentsTime = ?,contentsType = ?" +
+                            contentsCover +
+                            contentsFile +
+                            "where contentsNo = ?;",
+                        [
+                            categoryNo,
+                            contentsTitle,
+                            contentsExplain,
+                            contentsTime,
+                            contentsType,
+                            contentsNo
+                        ],
+                        (err, result, fields) => {
+                            if (err) {
+                                //에러체크
+                                return res
+                                    .status(400)
+                                    .json(
+                                        util.successFalse(err, "업데이트 실패")
+                                    );
+                            }
+
+                            if (result && result.length != 0) {
+                                //result 결과값이 있으면
+
+                                console.log(result);
+                                return res
+                                    .status(200)
+                                    .json(util.successTrue(result));
+                            } else {
+                                return res.sendStatus(204);
+                            }
+                        }
+                    );
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        } else {
+            con.query(
+                "update contents set categoryNo = ?,contentsTitle = ?,contentsExplain = ?,contentsTime = ?,contentsType = ?" +
+                    contentsCover +
+                    contentsFile +
+                    "where contentsNo = ?;",
+                [
+                    categoryNo,
+                    contentsTitle,
+                    contentsExplain,
+                    contentsTime,
+                    contentsType,
+                    contentsNo
+                ],
+                (err, result, fields) => {
+                    if (err) {
+                        //에러체크
+                        return res
+                            .status(400)
+                            .json(util.successFalse(err, "업데이트 실패"));
+                    }
+
+                    if (result && result.length != 0) {
+                        //result 결과값이 있으면
+
+                        console.log(result);
+                        return res.status(200).json(util.successTrue(result));
+                    } else {
+                        return res.sendStatus(204);
+                    }
+                }
+            );
+        }
+    }
+);
+
+router.put("/text", upload.single("text"), (req, res, next) => {
+    res.header("Access-Control-Allow-Headers", "multipart/form-data");
+    var {
+        categoryNo,
+        contentsTitle,
+        contentsExplain,
+        //contentsFile,
+        contentsType,
+        contentsNo
+    } = req.body;
+    /*
+    if(contentsType !== "sound" && contentsType !== "text"){
+        return res
+            .status(400)
+            .json(
+                util.successFalse(
+                    err,
+                    "지정되지 않은 콘텐츠 타입을 사용했습니다"
+                )
+            );
+    }
+    */
+    contentsType = "text";
+
+    var contentsFile = "";
+
+    if (req.file != undefined) {
+        contentsFile =
+            ", contentsFile = '" + req.file.filename.normalize("NFC") + "' ";
+    }
+
+    var contentsTime = "00:00:00";
+    console.log(req.files);
+
+    console.log("type is text");
+    con.query(
+        "update contents set categoryNo = ?,contentsTitle = ?,contentsExplain = ?,contentsTime = ?,contentsType = ?" +
+            contentsFile +
+            "where contentsNo = ?;",
+        [
+            categoryNo,
+            contentsTitle,
+            contentsExplain,
+            contentsTime,
+            contentsType,
+            contentsNo
+        ],
+        (err, result, fields) => {
+            if (err) {
+                //에러체크
+                return res
+                    .status(400)
+                    .json(util.successFalse(err, "업데이트 실패"));
+            }
+
+            if (result && result.length != 0) {
+                //result 결과값이 있으면
+
+                console.log(result);
+                return res.status(200).json(util.successTrue(result));
+            } else {
+                return res.sendStatus(204);
+            }
+        }
+    );
+});
+/*
 router.put("/", upload.single("contents"), (req, res, next) => {
     res.header("Access-Control-Allow-Headers", "multipart/form-data");
     const {
@@ -429,11 +638,9 @@ router.put("/", upload.single("contents"), (req, res, next) => {
         //const contentsTime = "00:12:23"; //파일을 받아서 시간 확인 필요
     }
 });
-
+*/
 router.delete("/", (req, res, next) => {
-    const {
-        contentsNo
-    } = req.body;
+    const { contentsNo } = req.body;
 
     con.query(
         "delete from contents where contentsNo = ?;",
@@ -458,7 +665,11 @@ router.delete("/", (req, res, next) => {
     );
 });
 
-router.options("/", (req, res) => {
+router.options("/sound", (req, res) => {
+    res.sendStatus(200);
+});
+
+router.options("/text", (req, res) => {
     res.sendStatus(200);
 });
 
@@ -468,6 +679,18 @@ router.options("/list", (req, res) => {
 
 router.options("/category/list", (req, res) => {
     res.sendStatus(200);
+});
+
+router.all("/sound", (req, res, next) => {
+    return res
+        .status(405)
+        .json(util.successFalse(null, "요청 메서드를 확인하세요"));
+});
+
+router.all("/text", (req, res, next) => {
+    return res
+        .status(405)
+        .json(util.successFalse(null, "요청 메서드를 확인하세요"));
 });
 
 router.all("/", (req, res, next) => {
